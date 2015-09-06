beae87d3e54974c7e96233a1bf0002992d72b72c
chickahoona <chickahoona@gmx.de>
2015-09-06 13:22:45 +0200

    jsoneditor added for demo client and a sharing model strucutre implemented

--- password_manager_server/restapi/migrations/0001_initial.py
+++ password_manager_server/restapi/migrations/0001_initial.py
@@ -28,13 +28,58 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Data_Store_Owner',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
                 ('create_date', models.DateTimeField(auto_now_add=True)),
                 ('write_date', models.DateTimeField(auto_now=True)),
                 ('email', models.EmailField(unique=True, max_length=254, verbose_name='email address')),
                 ('authkey', models.CharField(max_length=128, verbose_name='auth key')),
                 ('is_email_active', models.BooleanField(default=False, help_text='Designates whether this email should be treated as active. Unselect this if the user registers a new email.', verbose_name='email active')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this owner should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('create_date', models.DateTimeField(auto_now_add=True)),
+                ('write_date', models.DateTimeField(auto_now=True)),
+                ('owner', models.ForeignKey(related_name='group', to='restapi.Data_Store_Owner')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='Group_User_Right',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('create_date', models.DateTimeField(auto_now_add=True)),
+                ('write_date', models.DateTimeField(auto_now=True)),
+                ('read', models.BooleanField(default=True, help_text='Designates whether this user has "read" rights and can read shares of this group', verbose_name='read right')),
+                ('write', models.BooleanField(default=False, help_text='Designates whether this user has "write" rights and can update shares of this group', verbose_name='wright right')),
+                ('add_share', models.BooleanField(default=False, help_text='Designates whether this user has "add share" rights and can add shares to this group', verbose_name='add share right')),
+                ('remove_share', models.BooleanField(default=False, help_text='Designates whether this user has "remove share" rights and can remove shares of this group', verbose_name='remove share right')),
+                ('grant', models.BooleanField(default=False, help_text='Designates whether this user has "grant" rights and can add users and rights of users of thisgroup. The user is limited by his own rights, so e.g. he cannot grant write if he does not have write on his own.', verbose_name='grant right')),
+                ('revoke', models.BooleanField(default=False, help_text='Designates whether this user has "revoke" rights and can remove users and rights of users of this group. The owner of this group will always have full rights and cannot be shut out.', verbose_name='revoke right')),
+                ('group', models.ForeignKey(related_name='group_user_right', to='restapi.Group')),
+                ('user', models.ForeignKey(related_name='group_user_right', to='restapi.Data_Store_Owner')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='Share',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('create_date', models.DateTimeField(auto_now_add=True)),
+                ('write_date', models.DateTimeField(auto_now=True)),
+                ('data', models.BinaryField()),
+                ('type', models.CharField(default=b'password', max_length=64, db_index=True)),
+                ('owner', models.ForeignKey(related_name='share', to='restapi.Data_Store_Owner', help_text='The share owner is always the same as the group owner, so the group owner always keeps full control.')),
             ],
             options={
                 'abstract': False,
@@ -50,6 +95,11 @@ class Migration(migrations.Migration):
             options={
                 'abstract': False,
             },
+        ),
+        migrations.AddField(
+            model_name='group',
+            name='shares',
+            field=models.ManyToManyField(related_name='groups', to='restapi.Share'),
         ),
         migrations.AddField(
             model_name='data_store',
--- password_manager_server/restapi/models.py
+++ password_manager_server/restapi/models.py
@@ -12,6 +12,7 @@ class Data_Store_Owner(models.Model):
     """
     The custom user who owns the data storage
     """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     create_date = models.DateTimeField(auto_now_add=True)
     write_date = models.DateTimeField(auto_now=True)
     email = models.EmailField(_('email address'), unique=True)
@@ -27,10 +28,11 @@ class Data_Store_Owner(models.Model):
     class Meta:
         abstract = False
 
-    def is_authenticated(self):
+    @staticmethod
+    def is_authenticated():
         """
         Always return True. This is a way to tell if the user has been
-        authenticated in templates.
+        authenticated.
         """
         return True
 
@@ -46,6 +48,81 @@ class Data_Store(models.Model):
     data = models.BinaryField()
     type = models.CharField(max_length=64, db_index=True, default='password')
     description = models.CharField(max_length=64, default='default')
+
+    class Meta:
+        abstract = False
+
+
+class Share(models.Model):
+    """
+    The share objects for shares between users. All data encoded.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    write_date = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(Data_Store_Owner, on_delete=models.CASCADE, related_name='share',
+                              help_text=_('The share owner is always the same as the group owner, so the group '
+                                          'owner always keeps full control.'))
+    data = models.BinaryField()
+    type = models.CharField(max_length=64, db_index=True, default='password')
+
+    class Meta:
+        abstract = False
+
+
+class Group(models.Model):
+    """
+    The group object is the grouping object that glues shares and user rights together, the owner of the group
+    automatically owns all shares. A share can only be shared with a second group, if the group owners are identical.
+    If a share is shared with another person, a new group is created with the owner of the first group. This behaviour
+    ensures full control for the group owner.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    write_date = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(Data_Store_Owner, on_delete=models.CASCADE, related_name='group')
+    shares = models.ManyToManyField(Share, related_name='groups')
+
+    class Meta:
+        abstract = False
+
+
+class Group_User_Right(models.Model):
+    """
+    The group user rights objects for to define rights for group of users and shares.
+
+        read: Designates whether this user has "read" rights and can read shares of this group
+        write: Designates whether this user has "write" rights and can update shares of this group
+        add_share: Designates whether this user has "add share" rights and can add shares to this group
+        remove_share: Designates whether this user has "remove share" rights and can remove shares of this group
+        grant: Designates whether this user has "grant" rights and can add users and rights of users of this
+            group. The user is limited by his own rights, so e.g. he cannot grant write if he does not have
+            write on his own.
+        revoke: Designates whether this user has "revoke" rights and can remove users and rights of users of
+            this group. The owner of this group will always have full rights and cannot be shut out.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    create_date = models.DateTimeField(auto_now_add=True)
+    write_date = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(Data_Store_Owner, on_delete=models.CASCADE, related_name='group_user_right')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='group_user_right')
+
+    read = models.BooleanField(_('read right'), default=True,
+        help_text=_('Designates whether this user has "read" rights and can read shares of this group'))
+    write = models.BooleanField(_('wright right'), default=False,
+        help_text=_('Designates whether this user has "write" rights and can update shares of this group'))
+    add_share = models.BooleanField(_('add share right'), default=False,
+        help_text=_('Designates whether this user has "add share" rights and can add shares to this group'))
+    remove_share = models.BooleanField(_('remove share right'), default=False,
+        help_text=_('Designates whether this user has "remove share" rights and can remove shares of this group'))
+    grant = models.BooleanField(_('grant right'), default=False,
+        help_text=_('Designates whether this user has "grant" rights and can add users and rights of users of this'
+                    'group. The user is limited by his own rights, so e.g. he cannot grant write if he does not have '
+                    'write on his own.'))
+    revoke = models.BooleanField(_('revoke right'), default=False,
+        help_text=_('Designates whether this user has "revoke" rights and can remove users and rights of users of '
+                    'this group. The owner of this group will always have full rights and cannot be shut out.'))
+
 
     class Meta:
         abstract = False
