e19ec2e88f7bdd0b00453d2cc72b4d0d4ef06dc0
chickahoona <chickahoona@gmx.de>
2017-11-26 09:38:22 +0100

    prevent group names with "@"

--- psono/restapi/serializers/update_group.py
+++ psono/restapi/serializers/update_group.py
@@ -15,12 +15,21 @@ class UpdateGroupSerializer(serializers.Serializer):
         group_id = attrs.get('group_id')
         name = attrs.get('name', False)
 
+        if len(name) < 3:
+            msg = _('Name may not be shorter than 3 chars.')
+            raise exceptions.ValidationError(msg)
+
+        if '@' in name:
+            msg = _('Name may not contain an "@"')
+            raise exceptions.ValidationError(msg)
+
         # Lets check if the current user can do that
         try:
             membership = User_Group_Membership.objects.select_related('group').get(user=self.context['request'].user, group_id=group_id, group_admin=True, accepted=True)
         except User_Group_Membership.DoesNotExist:
             msg = _("You don't have permission to access or it does not exist.")
             raise exceptions.ValidationError(msg)
+
 
         attrs['group'] = membership.group
         attrs['name'] = name
