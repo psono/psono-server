c736158b43230f365bb7c0024f822cda966864f2
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-02-20 19:01:27 +0100

    removed docker hub security scan image

---
+++ psono/restapi/management/commands/cleanup.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.db.models import Exists, OuterRef
+from django.utils import timezone
+from restapi.models import Token, Secret, Secret_Link
+
+class Command(BaseCommand):
+    help = 'Clears expired token and objects (shares, secrets, ...) without reference'
+
+    def handle(self, *args, **options):
+
+        Token.objects.filter(valid_till__lt=timezone.now()).delete()
+
+        secret_links = Secret_Link.objects.filter(
+            secret = OuterRef('pk'),
+        )
+        Secret.objects.annotate(
+            has_secret_link = ~Exists(secret_links),
+        ).filter(has_secret_link=True).delete()
+
+        self.stdout.write('Done' )
