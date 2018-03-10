274fe0357c60586ce7bc49ff86a2c9ba170183ae
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-03-10 08:10:50 +0100

    Moved REPLAY and DEVICE protection to authenticate
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- psono/restapi/authentication.py
+++ psono/restapi/authentication.py
@@ -1,14 +1,30 @@
-from rest_framework.authentication import BaseAuthentication, get_authorization_header
-from rest_framework import exceptions
-from .models import Token, User
 from django.utils.translation import ugettext_lazy as _
-from .utils import get_cache, set_cache
-from hashlib import sha512
-from django.utils import timezone
+from django.utils.six import text_type
 from django.conf import settings
-from datetime import timedelta
-from django.core.cache import cache
+from django.utils import timezone
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from rest_framework import HTTP_HEADER_ENCODING, exceptions
 
+from hashlib import sha512
+import json
+import dateutil.parser
+
+from .parsers import decrypt
+from .models import Token, User
+from .utils import get_cache
+
+
+def get_authorization_validator_header(request):
+    """
+    Return request's 'Authorization:' header, as a bytestring.
+
+    Hide some test client ickyness where the header can be unicode.
+    """
+    auth = request.META.get('HTTP_AUTHORIZATION_VALIDATOR', b'')
+    if isinstance(auth, text_type):
+        # Work around django test client oddness
+        auth = auth.encode(HTTP_HEADER_ENCODING)
+    return auth
 
 class TokenAuthentication(BaseAuthentication):
     """
@@ -41,6 +57,38 @@ class TokenAuthentication(BaseAuthentication):
         if not user.is_email_active:
             raise exceptions.AuthenticationFailed(_('Account not yet verified.'))
 
+
+        # TODO Activate later once all clients send the new security header
+
+        # token_validator_encrypted = self.get_token_validator(request)
+        # token_validator_json = decrypt(token.secret_key, token_validator_encrypted['text'], token_validator_encrypted['nonce'])
+        # token_validator = json.loads(token_validator_json.decode())
+        #
+        # if not settings.DEVICE_PROTECTION_DISABLED:
+        #     request_device_fingerprint = token_validator.get('request_device_fingerprint', False)
+        #     if not request_device_fingerprint:
+        #         token.delete()
+        #         raise exceptions.AuthenticationFailed('Device Fingerprint Protection: request_device_fingerprint missing')
+        #     if str(request_device_fingerprint) != token.device_fingerprint:
+        #         token.delete()
+        #         raise exceptions.AuthenticationFailed('Device Fingerprint Protection: device_fingerprint mismatch')
+        # if not settings.REPLAY_PROTECTION_DISABLED:
+        #
+        #     client_date = token.client_date
+        #     create_date = token.create_date
+        #     request_date = token_validator.get('request_time', False)
+        #     now = timezone.now()
+        #
+        #     if not request_date:
+        #         token.delete()
+        #         raise exceptions.AuthenticationFailed('Replay Protection: request_time missing')
+        #
+        #     request_date = dateutil.parser.parse(request_date)
+        #     time_difference = abs(((client_date - create_date) - (request_date - now)).total_seconds())
+        #     if time_difference > settings.REPLAY_PROTECTION_TIME_DFFERENCE:
+        #         token.delete()
+        #         raise exceptions.AuthenticationFailed('Replay Protection: Time difference too big')
+
         request.user = user
         user.session_secret_key = token.secret_key
 
@@ -70,6 +118,32 @@ class TokenAuthentication(BaseAuthentication):
             msg = _('Invalid token header. Token string should not contain invalid characters.')
             raise exceptions.AuthenticationFailed(msg)
         return TokenAuthentication.user_token_to_token_hash(token)
+
+
+    @staticmethod
+    def get_token_validator(request):
+        auth = get_authorization_validator_header(request)
+        try:
+            auth = auth.decode()
+        except UnicodeError:
+            msg = _('Invalid token header. Token string should not contain invalid characters.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        try:
+            auth = json.loads(auth)
+        except ValueError:
+            msg = _('Invalid token header. Incorrect format in token header.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        if 'text' not in auth:
+            msg = _('Invalid token header. Token attribute not present.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        if 'nonce' not in auth:
+            msg = _('Invalid token header. Token attribute not present.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        return auth
 
     def get_db_token(self, token_hash):
 
--- psono/restapi/parsers.py
+++ psono/restapi/parsers.py
@@ -6,15 +6,12 @@ on the request, such as form content or json encoded data.
 """
 from __future__ import unicode_literals
 
-from django.conf import settings
-from django.utils import timezone
 from rest_framework.parsers import JSONParser
 from rest_framework import renderers
-from rest_framework.exceptions import AuthenticationFailed, ParseError
+from rest_framework.exceptions import ParseError
 import nacl.encoding
 import nacl.secret
 import json
-import dateutil.parser
 
 def decrypt(session_secret_key, text_hex, nonce_hex):
 
@@ -50,34 +47,5 @@ class DecryptJSONParser(JSONParser):
             data = json.loads(decrypted_data.decode())
         except ValueError:
             raise ParseError('Invalid request')
-
-        # TODO Activate later once all clients send the request_device_fingerprint
-        # if not settings.DEVICE_PROTECTION_DISABLED:
-        #     request_device_fingerprint = data.get('request_device_fingerprint', False)
-        #     if not request_device_fingerprint:
-        #         stream.auth.delete()
-        #         raise AuthenticationFailed('Device Fingerprint Protection: request_device_fingerprint missing')
-        #     if str(request_device_fingerprint) != stream.auth.device_fingerprint:
-        #         stream.auth.delete()
-        #         raise AuthenticationFailed('Device Fingerprint Protection: device_fingerprint mismatch')
-
-
-
-        if not settings.REPLAY_PROTECTION_DISABLED:
-
-            client_date = stream.auth.client_date
-            create_date = stream.auth.create_date
-            request_date = data.get('request_time', False)
-            now = timezone.now()
-
-            if not request_date:
-                stream.auth.delete()
-                raise AuthenticationFailed('Replay Protection: request_time missing')
-
-            request_date = dateutil.parser.parse(request_date)
-            time_difference = abs(((client_date - create_date) - (request_date - now)).total_seconds())
-            if time_difference > settings.REPLAY_PROTECTION_TIME_DFFERENCE:
-                stream.auth.delete()
-                raise AuthenticationFailed('Replay Protection: Time difference too big')
 
         return data
