8f2b370f1b5c51b82d5178940e9579f2ee9493e6
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2018-03-14 11:33:15 +0100

    testing changelog deploy
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

---
+++ var/deploy_changelog.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update && sudo apt-get -y install google-cloud-sdk
+
+
+echo "$GOOGLE_APPLICATION_CREDENTIALS" > "/root/key.json"
+gcloud auth activate-service-account --key-file=/root/key.json
+
+curl -H "PRIVATE-TOKEN: $GITLAB_PERSONAL_ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/repository/tags" --output changelog.json
+
+gsutil cp changelog.json gs://static.psono.com/gitlab.com/$CI_PROJECT_PATH/changelog.json
