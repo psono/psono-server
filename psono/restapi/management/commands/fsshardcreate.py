ceecac030ed6dd5621bbb1c41c9d8457e888f35a
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2019-05-18 10:49:18 +0200

    optimized some commands so they can be better scripted
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- psono/restapi/management/commands/fsshardcreate.py
+++ psono/restapi/management/commands/fsshardcreate.py
@@ -1,13 +1,18 @@
 from django.core.management.base import BaseCommand
 from restapi.models import Fileserver_Shard
+import sys
 
 
-def create_shard(title: str, description: str) -> dict:
+def create_shard(title: str, description: str, fix_shard_id=None) -> dict:
 
-    shard = Fileserver_Shard.objects.create(
-        title=title,
-        description=description,
-    )
+    try:
+        shard = Fileserver_Shard.objects.get(pk=fix_shard_id)
+    except Fileserver_Shard.DoesNotExist:
+        shard = Fileserver_Shard.objects.create(
+            pk=fix_shard_id,
+            title=title,
+            description=description,
+        )
 
     return {
         'shard': shard
@@ -21,19 +26,36 @@ class Command(BaseCommand):
         parser.add_argument('title', nargs='+')
         parser.add_argument('description', nargs='+')
 
+        parser.add_argument(
+            '--fix-shard-id',
+            type=str,
+            dest='fix_shard_id',
+            help='A custom fix shard id to use. Won\'t create anything if a shard with this id already exists., e.g. 3d594dcb-d14b-4b50-8247-38097ac9a2fd',
+        )
+
+        parser.add_argument(
+            '--simple-print',
+            action='store_true',
+            dest='simple_print',
+            help='Prints only the new shard id, e.g. 3d594dcb-d14b-4b50-8247-38097ac9a2fd',
+        )
+
 
     def handle(self, *args, **options):
 
         title = str(options['title'][0])
         description = str(options['description'][0])
 
-        result = create_shard(title, description)
+        result = create_shard(title, description, options['fix_shard_id'])
 
         if 'error' in result:
             self.stdout.write(result['error'])
-            return
+            sys.exit(1)
 
-        print('Created. Shard ID: ' + str(result['shard'].id))
+        if options['simple_print']:
+            print(result['shard'].id)
+        else:
+            print('Created. Shard ID: ' + str(result['shard'].id))
 
 
 
