67889150cfe836c993cea4eb6efaf3f9d451f6ca
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2021-02-13 12:49:08 +0100

    minor adjustments
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- psono/restapi/serializers/read_api_key_inspect.py
+++ psono/restapi/serializers/read_api_key_inspect.py
@@ -1,6 +1,6 @@
-from rest_framework import serializers
+from rest_framework import serializers, exceptions
 from ..fields import UUIDField
-from ..models import API_Key_Secret
+from ..models import API_Key_Secret, API_Key
 
 
 class ReadAPIKeyInspectSerializer(serializers.Serializer):
@@ -10,8 +10,15 @@ class ReadAPIKeyInspectSerializer(serializers.Serializer):
 
         api_key_id = attrs.get('api_key_id')
 
-        api_key_secrets = API_Key_Secret.objects.filter(api_key_id=api_key_id, api_key__active=True, api_key__user__is_active=True).only('secret_id').all()
+        try:
+            api_key = API_Key.objects.select_related('user').get(id=api_key_id, active=True, user__is_active=True)
+        except API_Key.DoesNotExist:
+            msg = "NO_PERMISSION_OR_NOT_EXIST"
+            raise exceptions.ValidationError(msg)
+
+        api_key_secrets = API_Key_Secret.objects.filter(api_key_id=api_key_id).only('secret_id').all()
 
         attrs['api_key_secrets'] = api_key_secrets
+        attrs['api_key'] = api_key
 
         return attrs
