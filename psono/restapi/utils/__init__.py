58bdbe7852969815c41fdc8e2f0ef2fbdaee2300
Sascha Pfeiffer <sascha.pfeiffer@psono.com>
2021-07-11 19:15:16 +0200

    minor change
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@psono.com>

--- psono/restapi/utils/__init__.py
+++ psono/restapi/utils/__init__.py
@@ -3,7 +3,7 @@ from .yubikey import *
 from .duo import *
 from .gcp import *
 from .aws import *
-from .azure_blob import *
+from .azure_blob import azure_blob_construct_signed_upload_url, azure_blob_construct_signed_download_url, azure_blob_delete
 from .do import *
 from .backblaze import *
 from .s3 import *
