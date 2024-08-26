0b802d92be6807aba2d9ac112a9757fedb0132eb
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2024-08-26 11:23:19 +0200

    Added validation for the EMAIL_SECRET_SALT parameter in the sendtestmail command
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- psono/restapi/management/commands/sendtestmail.py
+++ psono/restapi/management/commands/sendtestmail.py
@@ -1,15 +1,24 @@
+import bcrypt
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
 from django.conf import settings
 
 class Command(BaseCommand):
-    help = 'Sends a testmail to the specified email address'
+    help = 'Sends a testmail to the specified email address and validates EMAIL_SECRET_SALT'
 
     def add_arguments(self, parser):
         parser.add_argument('target_email', nargs='+')
 
     def handle(self, *args, **options):
         target_email = options['target_email'][0]
+
+        try:
+            bcrypt.hashpw(target_email.encode(), settings.EMAIL_SECRET_SALT.encode())
+        except:
+            self.stdout.write('Error EMAIL_SECRET_SALT incorrect. Make sure to use the generateserverkeys command to '
+                              'generate it (together with the other parameters). These parameters need to fulfill certain '
+                              'constraints and as such cannot be chosen manually.')
+            return
 
         content = 'If you read this, then everything is correctly configured.'
 
