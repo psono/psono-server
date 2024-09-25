71830269bd60546d0b6372c39dea81d6b77c4cf4
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2024-09-25 19:33:00 +0200

    Added info of forced groups to status endpoint
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- psono/restapi/views/status.py
+++ psono/restapi/views/status.py
@@ -43,6 +43,7 @@ class StatusView(GenericAPIView):
         if user_status is None:
             unaccepted_shares_count = User_Share_Right.objects.filter(user=request.user, accepted__isnull=True).exclude(creator__isnull=True).count()
             unaccepted_groups_count = User_Group_Membership.objects.filter(user=request.user, accepted__isnull=True).count()
+            unaccepted_forced_groups_count = User_Group_Membership.objects.filter(user=request.user, accepted__isnull=True, group__forced_membership=True).count()
 
             try:
                 latest_security_report = SecurityReport.objects.filter(user=request.user).latest('create_date')
@@ -57,6 +58,7 @@ class StatusView(GenericAPIView):
             user_status = {
                 'unaccepted_shares_count': unaccepted_shares_count,
                 'unaccepted_groups_count': unaccepted_groups_count,
+                'unaccepted_forced_groups_count': unaccepted_forced_groups_count,
                 'last_security_report_created': last_security_report_created,
             }
 
