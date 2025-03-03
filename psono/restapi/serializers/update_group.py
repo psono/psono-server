5e4350cfd33d32faccdb926dd920437f6ace8ae7
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2025-03-03 08:34:58 +0100

    Minor adjustment
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- psono/restapi/serializers/update_group.py
+++ psono/restapi/serializers/update_group.py
@@ -1,5 +1,3 @@
-from django.utils.translation import gettext_lazy as _
-
 from rest_framework import serializers, exceptions
 from ..fields import UUIDField
 from ..models import User_Group_Membership
@@ -7,19 +5,19 @@ from ..models import User_Group_Membership
 class UpdateGroupSerializer(serializers.Serializer):
 
     group_id = UUIDField(required=True)
-    name = serializers.CharField(max_length=64, required=False)
+    name = serializers.CharField(max_length=64, required=True)
 
     def validate(self, attrs: dict) -> dict:
 
         group_id = attrs.get('group_id')
-        name = attrs.get('name', False)
+        name = attrs.get('name')
 
         if len(name) < 3:
-            msg = _('Name may not be shorter than 3 chars.')
+            msg = 'NAME_TOO_SHORT'
             raise exceptions.ValidationError(msg)
 
         if '@' in name:
-            msg = _('Name may not contain an "@"')
+            msg = 'NAME_CONTAINS_FORBIDDEN_CHARACTERS'
             raise exceptions.ValidationError(msg)
 
         # Lets check if the current user can do that
